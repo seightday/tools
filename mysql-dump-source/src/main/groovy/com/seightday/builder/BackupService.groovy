35c93704945a7ccd1d36e96c5dfb9c37e5f5521a
seightday <seightday@gmail.com>
2017-08-06 15:03:57 +0800

    优化

--- mysql-dump-source/src/main/groovy/com/seightday/builder/BackupService.groovy
+++ mysql-dump-source/src/main/groovy/com/seightday/builder/BackupService.groovy
@@ -18,7 +18,7 @@ class BackupService implements CommandLineRunner{
 	@Value('${part.data.tables}')
 	String[] partDataTables
 	@Value('${ignore.tables}')
-	String[] ignoreTables
+	String ignoreTables
 	@Value('${db.name}')
 	String dbName
 	@Value('${db.host}')
@@ -27,17 +27,29 @@ class BackupService implements CommandLineRunner{
 	String sqlDir
 	@Value('${sql.source.dir}')
 	String sqlSourceDir
-	@Value('${mysqldump}')
-	String mysqldump
-	
+	@Value('${dump.exe}')
+	String dumpExe
+	@Value('${dump.params}')
+	String dumpParams
+
 	@Override
-	public void run(String... arg0) throws Exception {
+	void run(String... arg0) throws Exception {
+		//mysqldump不要锁表，不生成注释
+		//mysqldump --help
 		StringBuilder sb=new StringBuilder();
-		ignoreTables.each {
-			sb.append(" --ignore-table=${dbName}.$it ")
+		if (ignoreTables){
+			ignoreTables.split(',').each {
+				sb.append(" --ignore-table=${dbName}.$it ")
+			}
 		}
-		def dump="$mysqldump -u$dbUser -p$dbPwd -h $dbHost $dbName $sb > $sqlDir/all.sql"
+		partDataTables.each {
+			def split=it.split('##')
+			sb.append(" --ignore-table=${dbName}.${split[0]} ")
+		}
+
+		def dump="$dumpExe $dumpParams -u$dbUser -p$dbPwd -h $dbHost $dbName $sb > $sqlDir/all.sql"
 		println(dump)
+		//mysqldump -t -uroot -pwtsd123 -h 192.168.0.185 sms --tables tb_sale_detail > C:\Users\Administrator\Desktop\saledetail.sql//只导指定表的数据，会锁表
 //		dump.execute()
 //		printProcess(dump.execute())
 		//mysqldump -uoms -poms -h 192.168.0.198 oms --ignore-table=oms.tb_clear_paydata_detail --ignore-table=oms.tb_biz_refund_record > C:\Users\Administrator\Desktop\data\ignore.sql
@@ -45,7 +57,7 @@ class BackupService implements CommandLineRunner{
 //		mysqldump -uoms -poms -h 192.168.0.198 oms tb_biz_refund_record --where="applytime>'2017-04-14 17:19:41'" > C:\Users\Administrator\Desktop\data\tb_biz_refund_record.sql
 		partDataTables.each {
 			def split=it.split('##')
-			def s="$mysqldump -u$dbUser -p$dbPwd -h $dbHost $dbName ${split[0]} --where=\"${split[1]}\" > $sqlDir/${split[0]}.sql"
+			def s="$dumpExe $dumpParams -u$dbUser -p$dbPwd -h $dbHost $dbName ${split[0]} --where=\"${split[1]}\" > $sqlDir/${split[0]}.sql"
 			println(s)
 //			s.execute()
 //			printProcess(s.execute())
