1ec3710c70530a5bd704ead4be9dca6608175cb6
seightday <seightday@gmail.com>
2018-05-06 21:43:06 +0800

    增加anki输出

--- kfyx-wx-test/src/main/java/ExamJson.java
+++ kfyx-wx-test/src/main/java/ExamJson.java
@@ -9,7 +9,7 @@ import java.util.List;
 /**
  * chrome打开开发者工具，模块手机打开以下网址
  * http://www.kfyx.cn/wx/test/
- * https://www.kfyx.cn/plugin.php?id=tpgao_m:m&mod=exam&item=80&formhash=f3defafc
+ * https://www.kfyx.cn/plugin.php?id=tpgao_m:m
  * 复制html到test/textName.txt
  */
 public class ExamJson {
@@ -18,32 +18,67 @@ public class ExamJson {
 
         String[] abcdefg={"A","B","C","D","E","F","G"};
 
-        String testName="[试卷][209康复治疗技术初级师]2017年专业实践能力模拟试题";
-        File output = new File("C:/Users/Administrator/Desktop/output/"+testName+".txt");
-        File outputAnswer = new File("C:/Users/Administrator/Desktop/output/"+testName+"-答案.txt");
+        String testName="[试卷]S17Z348康复医学中级2017年专业知识模拟试题";
+        String dir = "D:/微云同步助手/674114624/kfyx/【康复医学中级348】2017年模拟试题(4套)/";
+        File outputAsk = new File(dir +testName+"-题目.txt");
+        if(outputAsk.exists()){
+            outputAsk.delete();
+        }
+        File outputAnswer = new File(dir +testName+"-答案.txt");
+        if(outputAnswer.exists()){
+            outputAnswer.delete();
+        }
+        File outputAskAnswer = new File(dir +testName+"-题目+答案.txt");
+        if(outputAskAnswer.exists()){
+            outputAskAnswer.delete();
+        }
+        File outputAnki = new File(dir +testName+"-anki.txt");
+        if(outputAnki.exists()){
+            outputAnki.delete();
+        }
 
         String encoding = "gbk";
-        String fileToString = FileUtils.readFileToString(new File("C:/Users/Administrator/Desktop/test/" + testName + ".txt"), encoding);
+        String fileToString = FileUtils.readFileToString(new File(dir + testName + ".txt"), encoding);
         fileToString=fileToString.replaceAll("．",".");
         List<HashMap> mapList = JSON.parseArray(fileToString, HashMap.class);
         for (int i = 0; i < mapList.size(); i++) {
             HashMap hashMap = mapList.get(i);
+
+            String askAndAnswer="";
+            String anki="";
+
             String ask = hashMap.get("ask").toString();
-            FileUtils.write(output, ask +"\r\n", encoding,true);
+            FileUtils.write(outputAsk, ask +"\r\n", encoding,true);
+            askAndAnswer+=ask +"\r\n";
+            anki+=ask+"<br>";
             for (int j = 0; j < 7; j++) {
                 Object o = hashMap.get("option" + (j + 1));
                 if(o!=null && !o.toString().isEmpty()){
-                    FileUtils.write(output,abcdefg[j]+":"+o.toString()+"\r\n", encoding,true);
+                    FileUtils.write(outputAsk,abcdefg[j]+":"+o.toString()+"\r\n", encoding,true);
+                    askAndAnswer+=abcdefg[j]+":"+o.toString()+"\r\n";
+                    anki+=abcdefg[j]+":"+o.toString()+"<br>";
                 }
             }
+            FileUtils.write(outputAsk, "\r\n", encoding,true);
+
             String[] split = ask.split("\\.");
             Object answer = hashMap.get("answer");
             FileUtils.write(outputAnswer,split[0]+":"+answer, encoding,true);
+            askAndAnswer+="答案:"+answer;
+            anki+="\t答案:"+answer;
             Object answer_parser = hashMap.get("answer_parser");
             if(answer_parser!=null&&!answer_parser.toString().isEmpty()&&!answer_parser.toString().contains("暂无解析")){
-                FileUtils.write(outputAnswer,"。解释："+answer_parser, encoding,true);
+                FileUtils.write(outputAnswer,"。\r\n解释："+answer_parser, encoding,true);
+                askAndAnswer+="。\r\n解释："+answer_parser;
+                anki+="。<br>解释："+answer_parser;
             }
             FileUtils.write(outputAnswer,"\r\n", encoding,true);
+            askAndAnswer+="\r\n\r\n";
+            anki+="\r\n";
+
+            FileUtils.write(outputAskAnswer,askAndAnswer, encoding,true);
+
+            FileUtils.write(outputAnki,anki, "utf-8",true);
         }
     }
 }
